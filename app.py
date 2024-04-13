f7988ba71ab021c700664629fcbdd84c3386c3ef
Jakub Michalski <130910084+jakub1m@users.noreply.github.com>
2024-04-13 14:43:21 +0200

    .

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ def analyze_sentiment() -> Dict[str, Union[float, str]]:
 
  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000, threaded=True)
+    app.run(host='0.0.0.0', debug=False, port=5000, threaded=True)
